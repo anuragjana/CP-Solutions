527788dc77d9744d73d484555909e77145a47edb
anuragjana <80119836+anuragjana@users.noreply.github.com>
2022-09-04 16:36:11 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1707A | Doremy's IQ/170400678.cpp
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<climits>
+#include<string>
+using namespace std;
+ 
+// Macros 
+#define vi              vector<int>
+#define vll             vector<ll>
+#define pll             pair<ll, ll>
+#define pii             pair<int, int>
+#define ld              long double 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define vs              vector<string>
+#define vpll            vector<pll>
+#define vb              vector<bool>
+#define mll             map<ll,ll> 
+#define mii             map<int,int>
+#define endl            '\n'
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define f(i,a,b)        for(int i=a;i<=b;i++)
+#define fb(i,a,b)       for(int i=a;i>=b;i--)
+#define mp              make_pair
+#define pb              push_back
+#define precise(n)      fixed << setprecision(n)<< 
+#define py              cout<<"YES"<<endl;
+#define pn              cout<<"NO"<<endl;
+#define pm              cout<<"-1"<<endl;
+#define all(arr)        arr.begin(), arr.end()
+#define ain(a,n) for(ll i=0;i<n;i++)cin>>(a)[i]
+#define show(a) for(auto x:a)cout<<x<<" ";
+ 
+ 
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+ 
+ 
+//Constants
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
