157e34a20ac8c035b427167b6326c10a48f8b2f1
anuragjana <80119836+anuragjana@users.noreply.github.com>
2022-09-04 16:54:41 +0530

    Committing files

---
+++ CodeForces/GNU C++17/80A | Panoramix's Prediction/140487523.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<climits>
+#include<string>
+using namespace std;
+ 
+#define vi              vector<int>
+#define vll             vector<ll>
+#define pll             pair<ll, ll>
+#define pii             pair<int, int>
+#define ld              long double 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define vs              vector<string>
+#define vpll            vector<pll>
+#define vb              vector<bool>
+#define mp              make_pair
+#define pb              push_back
+#define endl            '\n'
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define f(i,a,b)        for(int i=a;i<b;i++)
+#define M               1000000007
+#define pb              push_back
+bool isPrime(int n)
+{
+    for(int i=2;i<=n-1;i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int m,n;
+    cin>>n>>m;
+    bool nextPrime = true;
+    for(int i=n+1;i<m;i++)
+    {
+        if(isPrime(i))
+            nextPrime = false ;
+    }
+    if(nextPrime && isPrime(m))
+        cout<<"YES";
+    else
+        cout<<"NO";
+ 
+    return 0;
+}
+ 
