ec99c980aa95411e8f648dfd9bcde3beb4175ee0
anuragjana <80119836+anuragjana@users.noreply.github.com>
2022-09-04 16:54:47 +0530

    Committing files

---
+++ CodeForces/GNU C++17/281A | Word Capitalization/140484245.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<climits>
+#include<string>
+using namespace std;
+ 
+#define vi              vector<int>
+#define vll             vector<ll>
+#define pll             pair<ll, ll>
+#define pii             pair<int, int>
+#define ld              long double 
+#define ll              long long 
+#define ff              first
+#define ss              second
+#define vs              vector<string>
+#define vpll            vector<pll>
+#define vb              vector<bool>
+#define mp              make_pair
+#define pb              push_back
+#define endl            '\n'
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define f(i,a,b)        for(int i=a;i<b;i++)
+#define M               1000000007
+#define pb              push_back
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string s;
+    cin>>s;
+    char c = toupper(s[0]);
+    s[0] = c;
+    cout<<s;
+    return 0;
+}
+ 
